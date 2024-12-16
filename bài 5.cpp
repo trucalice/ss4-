649155d2247ee04dec9a24fd7c75a67923d6db6f
trucalice <nguyenthithanhhuong99992006@gmail.com>
2024-12-16 20:15:43 +0700

    Add files via upload

--- bài 5.cpp
+++ bài 5.cpp
@@ -1,20 +1,18 @@
 #include<stdio.h>
-
 int main(){
-	int a,b,c;	
-	printf("nhap a: ");
-	scanf("%d",&a);
-	printf("nhap b: ");
-	scanf("%d",&b);
-	printf("nhap c: ");
-	scanf("%d",&c);
-	if (c - a == 1 && c + 1 == b){
-		printf("%d%d%d",a,c,b);
-	}
-	else {
-	printf ("%d khong nam giua %d va %d",c,a,b);
+    int a, b, c;    
+    printf("Nhap a: ");
+    scanf("%d", &a);
+    printf("Nhap b: ");
+    scanf("%d", &b);
+    printf("Nhap c: ");
+    scanf("%d", &c);
+
+    if ((a < c && c < b) || (b < c && c < a)) {
+        printf("%d nam giua %d va %d\n", c, a, b);
+    } else {
+        printf("%d khong nam giua %d va %d\n", c, a, b);
+    }
+    return 0;
 }
-	return 0;
-}
-	
-	
+
