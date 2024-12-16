b97ace92001b52b548b44851a0af4cc0fe1b70ea
trucalice <nguyenthithanhhuong99992006@gmail.com>
2024-12-16 20:07:46 +0700

    Add files via upload

--- bài 3.cpp
+++ bài 3.cpp
@@ -1,15 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-	int n;
-	printf("hay nhap 1 so : ");
-	scanf("%d",&n);
-	if (n % 3 ==0 && n % 5 == 0){
-		printf("%d chia het cho 3 va 5",n );
-	}
-	else
-	if (n % 3 == 0 && n % 5 != 0){
-		printf("%d chia het cho 3 va 5 khong",n);
-	}
-	return 0;
+int main() {
+    int n;
+    printf("Hay nhap 1 so: ");
+    scanf("%d", &n);
+    if (n % 3 == 0 && n % 5 == 0) {
+        printf("%d chia het cho ca 3 va 5.\n", n);
+    } else if (n % 3 == 0) {
+        printf("%d chi chia het cho 3.\n", n);
+    } else if (n % 5 == 0) {
+        printf("%d chi chia het cho 5.\n", n);
+    } else {
+        printf("%d khong chia het cho ca 3 va 5.\n", n);
+    }
+
+    return 0;
 }
+
